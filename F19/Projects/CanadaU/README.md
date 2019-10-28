58a174533799f284325b844568a8e32062ad3f6c
Michael Eden <Mikey.Eden@gmail.com>
2019-10-28 17:55:37 -0400

    Make fixes to Lesson 1

---
+++ F19/Projects/CanadaU/README.md
@@ -0,0 +1,41 @@
+# CanadaU
+![CanadaU Logo](assets/logo.png "CanadaU")
+
+Hello and welcome to Coffee N Code!
+<br>
+My name is Michael, and I'll be guiding you through our project <i>CanadaU</i>.
+<br>
+
+### Join the [Coffee N Code Slack](https://bit.ly/uwcoffeencodeslack), then join the `#canada-u` channel for project updates.
+
+## Project Overview
+
+CanadaU is a website which gathers trends from Canadian university subreddits, and puts them all in one place.
+<br>
+The project is open-ended, so you can add your own ideas and functionality as you feel comfortable.
+
+Technologies we'll be using:
+- Python with the [Flask](https://palletsprojects.com/p/flask/) framework
+- JavaScript with the [React](https://reactjs.org) framework
+- [Reddit API](https://www.reddit.com/dev/api/)
+
+## Syllabus
+
+| Lesson | Date   | Content                               |
+|--------|--------|---------------------------------------|
+| 1      | Oct 9  | Try Reddit API                        |
+| 2      | Oct 30 | Create back end w/ Flask and Python   |
+| 3      | Nov 6  | Create front end w/ React (pt 1)      |
+| 4      | Nov 13 | Create front end w/ React (pt 2)      |
+| 5      | Nov 20 | Tying everything together             |
+| 6      | Nov 27 | Finishing touches                     |
+
+## Getting Started
+
+If you're new to the project, start with Lesson 1!
+
+## Troubleshooting or Questions
+
+If you run into any problems, or have any questions about the project, please reach out to me (Michael Eden) over Slack!
+<br>
+I'm more than happy to help :) 
--- F19/Projects/CanadaU/lesson1/praw_examples.py
+++ F19/Projects/CanadaU/lesson1/praw_examples.py
@@ -1,22 +1,55 @@
 import praw
 
-reddit = praw.Reddit(client_id='gkVVyUb93yf45Q',
-                     client_secret='r77NHUBX4lkTLQvjqtVUixFTEK8',
-                     user_agent='my user agent')
+# Quick start guide:
+# https://praw.readthedocs.io/en/latest/getting_started/quick_start.html
 
-for submission in reddit.subreddit('uwaterloo').hot(limit=10):
-    print(submission.title)
 
-canada_universities = [
-    "UofT",
-    "uwaterloo",
-    "uAlberta",
-    "UBC",
-    # And everyone's favourite
-    "nipissingu"
-]
 
-for university in canada_universities:
-    print("University: %s" % university)
-    for submission in reddit.subreddit(university).hot(limit=10):
-        print("\t %s" % submission.title)
+"""
+- - - CREATING REDDIT CLIENT - - -
+"""
+
+# NOTE: Fill in with your credentials where indicated by the '<>'
+reddit = praw.Reddit(client_id='<client ID>',
+                     client_secret='<client secret>',
+                     user_agent='web:CanadaU:1.0 (by /u/<reddit username>)')
+
+
+
+"""
+- - - SUBREDDIT OPERATIONS - - -
+"""
+subreddit = reddit.subreddit("uwaterloo")
+
+print("Subscriber Count: %d" % subreddit.subscribers)
+
+print("Hot")
+hot_submissions = subreddit.hot(limit=10)
+for submission in hot_submissions:
+    print("\t %s" % submission.title)
+
+print("Top of All Time")
+top_submissions = subreddit.top('all', limit=10)
+for submission in top_submissions:
+    print("\t %s" % submission.title)
+
+# For more examples, see:
+# https://praw.readthedocs.io/en/latest/code_overview/models/subreddit.html
+
+
+
+"""
+- - - SUBMISSION AND COMMENT OPERATIONS - - -
+"""
+print("Comments on Top Submission")
+top_submissions = subreddit.top('all', limit=10)
+top_submission = next(top_submissions)
+comments = top_submission.comments
+for comment in comments:
+    try:
+        print("\t %s" % comment.body)
+    except AttributeError:
+        pass
+
+# For more examples, see:
+# https://praw.readthedocs.io/en/latest/code_overview/other/commentforest.html
